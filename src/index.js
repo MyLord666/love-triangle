4e17610d9f1da34d279d0f0772c2535601b66cfc
Munis <munisjon1998@mail.ru>
2019-03-21 01:00:40 +0300

    Complate task love-triangle

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var count = 0;
+  for (var i = 0; i < preferences.length; i++) {
+    var firstSpichonee = preferences[i] - 1;
+    if(firstSpichonee === i) continue;
+
+    var secondSpichonee = preferences[firstSpichonee] - 1;
+    if(secondSpichonee === firstSpichonee) continue;
+
+    var thirdSpichonee = preferences[secondSpichonee] - 1;
+    if(thirdSpichonee === secondSpichonee) continue;
+
+    if(thirdSpichonee === i) count++;
+  }
+  return count/3;
 };
